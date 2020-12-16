23c2c2ab7b4da66f52d8d19ca2939afc478962ff
mayar-ali <mayarali1999@gmail.com>
2020-12-16 23:50:29 +0200

    added the login route

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ const bcrypt = require('bcyrptjs');
 const jst = require('jsonwebtoken');
 const { nextTick } = require('process');
 
+//-----------------MODELS----------------------------------------
+
+const Staff = require('./models/staff');
+const HR = require('./models/hr');
+const CC = require('./models/courseCoordinator');
+const CI = require('./models/courseInstructor');
+const TA = require('./models/ta');
+const HOD = require('./models/hod');
+
+//----------------END OF MODELS----------------------------------
+
 //TODO: save it in an env file?
 //key used for jsonwebtoken
 const jwtKey = "akjhfdkadsjhfdsjklalsdsfajhjkhsdfakjhfdsa";
@@ -75,6 +86,14 @@ mongoose.connect(mongooseURL, mongConnectionParams)
       console.log("Database failed to connect")
     }
   );
+
+//TODO: test
+//function that validates string as one following the email format
+function emailValidator(email)
+{
+  const re = /\S+@\S+\.\S+/;
+  return  re.test(email);
+}
 
 //Authentication middeware
 //Used to check if user has a valid jsonwebtoken (i.e. logged in)
@@ -149,6 +168,61 @@ app.post('/login', async (req, res) =>
         {
           return res.status(400).json({msg: "Please enter both email and passowrd"});
         }
+
+        //if email entered is not valid
+        if(!emailValidator(email))
+        {
+          return res.status(400).json({msg: "Please enter a valid email address"});
+        }
+
+        //TODO: validate password
+
+        //check if email exists in database
+        const existingStaff = await Staff.findOne({email: email});
+        //if there is no such email in the staff table
+        if(!existingStaff)
+        {
+          return res.status(400).json({msg: "Email is not registered"});
+        }
+
+        //compare password entered with the hashed one stored in the staff model
+        const passMatch = await bcrypt.compare(password, existingStaff.password);
+        //if password does not match
+        if(!passMatch)
+        {
+          return res.status(400).json({msg: "Incorrect password"});
+        }
+
+        //user passed all checks
+        //should log in and receive a token
+
+        //First find the role of the user
+
+        //get the id of the staff
+        const id = existingStaff.id;
+        //check if the prefix is hr- and therefore user is an HR member
+        if(id.startsWith("hr-"))
+        {
+          const role = "HR";
+        }
+        //else the prefix of the id is ac-
+        //no other prefixes are available
+        //registeration should not add an id with prefixes other than those two
+        else
+        {
+          //TODO: check with others: role or traverse all sub models of academic?
+          const role = "balabizo";
+        }
+
+        //fill the token payload with the staff id and role in uni
+        const payload = {id: id, role: role};
+        //create a token
+        const token = jwt.sign(payload, jwtKey);
+        //give the token to the user by adding it to the header of the response 
+        res.header({'auth-token': token});
+
+        //TODO: what to send to the user?
+        return res.json({msg: "Login Successful"});
       }
       catch(error)
       {

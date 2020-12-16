dd94a74ec3202143e24fdfa5fb8e8bab20ef1249
mayar-ali <mayarali1999@gmail.com>
2020-12-16 21:32:31 +0200

    refined the authentication and cc authorisation method

--- app.js
+++ app.js
@@ -41,22 +41,18 @@ app.use(function(err, req, res, next) {
 //------------------------------------------------------------------------
 //                    MAYAR'S TRASH
 //------------------------------------------------------------------------
-/*
+
 //mongoose to connect to database
 const mongoose = require('mongoose');
 //to encrypt and decrypt passwords
 const bcrypt = require('bcyrptjs');
 //required to store information about current user
 const jst = require('jsonwebtoken');
+const { nextTick } = require('process');
 
+//TODO: save it in an env file?
 //key used for jsonwebtoken
 const jwtKey = "akjhfdkadsjhfdsjklalsdsfajhjkhsdfakjhfdsa";
-
-//process request body as a JSON object
-app.use(express.json());
-//process request body as array/string
-//extended set to false as we don't need to parse the extended syntax
-app.use(express.urlencoded({extended:false}));
 
 //mongoose connection parameters to fix deprecation warnings
 const mongConnectionParams = 
@@ -81,20 +77,86 @@ mongoose.connect(mongooseURL, mongConnectionParams)
   );
 
 //Authentication middeware
-//Used to check if user has a jsonwebtoken (i.e. logged in)
-const authenticate = (req, res, next) =>
+//Used to check if user has a valid jsonwebtoken (i.e. logged in)
+function authenticate(req, res, next)
+{
+  //get the user's token
+  const token = req.header('auth-token');
+  //if there is no token
+  if(!token)
+  {
+    return res.status(403).json({msg: "You need to login first to access this page."});
+  }
+
+  try
+  {
+    //verify that the signature is legitimate using the jwt key
+    const verified = jwt.verify(token, key);
+    req.user = verified; //sends the payload to a request attribute named user
+    next(); //call the next function
+  }
+  catch(error) //catches an error should the token fail the verification
+  {
+    return res.status(500).json({msg: error.message});
+  }
+}
+
+//Authentication and authorisation middleware
+//Checks if user has a valid token and is course coordinator
+function authenticateAndAuthoriseCC(req, res, next)
+{
+  //get user's token
+  const token = req.header('auth-token');
+  //if there is no token
+  if(!token)
+  {
+    return res.status(403).json({msg: "You need to login to access this page"});
+  }
+
+  try
+  {
+    //verify the signature of the token is legitimate using the key
+    const verified = jwt.verify(token, key);
+    req.user = verified; //sends the payload to a request attribute named user
+
+    //gets the user's role in the university
+    const role = verified.role;
+    if(role === "Course Coordinator")
     {
-      const token = req.header('auth-token');
-      if
+      //if current user is a course coordinator, continue executing the next function
+      next();
     }
+    else
+    {
+      res.status(401).json({msg: "Unauthorized to access this page."});
+    }
+  }
+  catch(error) //catches error thrown should token fail verification
+  {
+    return res.status(500).json({msg: error.message});
+  }
+}
 
 //login route
 app.post('/login', async (req, res) =>
     {
+      try
+      {
+        const {email,password} = req.body;
 
+        //if user didn't enter email or password
+        if(!email || !password)
+        {
+          return res.status(400).json({msg: "Please enter both email and passowrd"});
+        }
+      }
+      catch(error)
+      {
+        res.status(500).json({msg: error.message});
+      }
     }
-)
-*/
+);
+
 //------------------------------------------------------------------------
 //                    END OF MAYAR'S TRASH
 //------------------------------------------------------------------------

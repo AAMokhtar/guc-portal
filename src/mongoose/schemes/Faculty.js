5c1b0ad4e3408ec66cfc1ff4eabdbb2ae740fb14
Abdelrahman khattab <abdelrahman@energenious.eu>
2020-12-17 22:02:41 +0200

    small edit

--- src/mongoose/schemes/Faculty.js
+++ src/mongoose/schemes/Faculty.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const Department = require("./Department");
 const schema = new Schema({
   name: {
     type: String,
     required: true,
     unique: true,
   },
-  departmentsIDs: [{ type: Schema.Types.ObjectId, ref: "Department" }],
+  departments: [Department],
 });
 module.exports = schema;
---
+++ src/routes/hod.js
@@ -0,0 +1,5 @@
+const app = require("../../app");
+app.get("/hod/assignCourse", async function (req, res) {
+   let {staffID,course} = req.body;
+   
+}

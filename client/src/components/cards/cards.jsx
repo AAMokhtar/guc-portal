76fba8a5d800752e2d626c75bcd166144c2edfde
abzokhattab <abdelrahman@energenious.eu>
2021-01-14 02:37:30 +0200

    small edits

--- client/src/components/cards/cards.jsx
+++ client/src/components/cards/cards.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import PropType from "prop-types";
-import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const Cards = (props) => {
   return (
-    <div className="card text-center" style={{width: '18rem'}}>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">
-      {props.text}.
-    </p>
-    <Link to={props.link} className="btn btn-primary">Open Link</Link>
-  </div>
-</div>  );
+    <div className="card text-center" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.text}.</p>
+        <Link to={props.link} className="btn btn-primary">
+          Open Link
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 Cards.propTypes = {
--- client/src/views/viewStaffRequests.jsx
+++ client/src/views/viewStaffRequests.jsx
@@ -1,38 +1,43 @@
 import React, { Component, setState } from "react";
-import { Container, Jumbotron, Button, Table, Col, Form, Modal } from "react-bootstrap";
+import {
+  Container,
+  Jumbotron,
+  Button,
+  Table,
+  Col,
+  Form,
+  Modal,
+} from "react-bootstrap";
 import faker from "faker";
 import SmartDataTable from "react-smart-data-table";
-const axios = require('axios');
-axios.defaults.headers.common['auth-token'] = localStorage.getItem('token');
+const axios = require("axios");
+axios.defaults.headers.common["auth-token"] = localStorage.getItem("token");
 
 class ViewStaffRequests extends Component {
   state = {
-      show: false,
-      persons: []
+    show: false,
+    persons: [],
   };
 
   componentDidMount() {
-    console.log("getting")
-    axios.get('http://localhost:4000/hod/viewStaff')
-      .then(res => {
-        console.log("resss", res)
-        const persons = res.data;
-        this.setState({ persons });
-      })
+    console.log("getting");
+    axios.get("http://localhost:4000/hod/viewStaff").then((res) => {
+      console.log("resss", res);
+      const persons = res.data;
+      this.setState({ persons });
+    });
   }
 
   render() {
     const handleShow = () => {
-        this.setState({show: true})
-    }
+      this.setState({ show: true });
+    };
 
     const handleClose = () => {
-        this.setState({show: false})
-    }
-
+      this.setState({ show: false });
+    };
 
     return (
-
       <Container>
         <Jumbotron>
           <h1>Staff Requests!</h1>
@@ -40,59 +45,84 @@ class ViewStaffRequests extends Component {
             Here you can see, accept or refuse staff requests
             {this.state.persons}
           </p>
-          
         </Jumbotron>
 
-      <Modal show={this.state.show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter your reject message</Modal.Title>
-        </Modal.Header>
-        <Modal.Body><Form> <Form.Group controlId="formBasicEmail">
-    <Form.Label>Response</Form.Label>
-    <Form.Control type="text" placeholder="Enter ur response" />
-    <Form.Text className="text-muted">
-      Response will be viewd by the staff.
-    </Form.Text>
-  </Form.Group></Form></Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        <Modal show={this.state.show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Enter your reject message</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              {" "}
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Response</Form.Label>
+                <Form.Control type="text" placeholder="Enter ur response" />
+                <Form.Text className="text-muted">
+                  Response will be viewd by the staff.
+                </Form.Text>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Type</th>
-      <th>Execuse</th>
-      <th>Decision</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td><Col><Button varient='success'>Accept</Button> <Button onClick={handleShow} variant='danger'>Reject</Button></Col></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td><Col><Button varient='success'>Accept</Button> <Button onClick={handleShow} variant='danger'>Reject</Button></Col></td>
-    </tr>
-    <tr>
-    <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td><Col><Button varient='success'>Accept</Button> <Button onClick={handleShow} variant='danger'>Reject</Button></Col></td>
-    </tr>
-  </tbody>
-</Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Type</th>
+              <th>Execuse</th>
+              <th>Decision</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>
+                <Col>
+                  <Button varient="success">Accept</Button>{" "}
+                  <Button onClick={handleShow} variant="danger">
+                    Reject
+                  </Button>
+                </Col>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>
+                <Col>
+                  <Button varient="success">Accept</Button>{" "}
+                  <Button onClick={handleShow} variant="danger">
+                    Reject
+                  </Button>
+                </Col>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>
+                <Col>
+                  <Button varient="success">Accept</Button>{" "}
+                  <Button onClick={handleShow} variant="danger">
+                    Reject
+                  </Button>
+                </Col>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     );
   }

f9f4a0a9c1dee3f6abe33d77e8be1f736274e714
Ahmed Ashraf <ahmed.abdelrehim@student.guc.edu.eg>
2021-01-17 09:29:18 +0200

    Update properties.js

--- server/properties.js
+++ server/properties.js
@@ -1,7 +1,6 @@
-const env = require("dotenv").config();
 
 module.exports = {
     CONNECTION_STRING: process.env.MONGODB_URI || 'mongodb+srv://admin:admin@omegacentauri.vydfb.mongodb.net/portalData?retryWrites=true&w=majority',
-    PORT:  env.PORT || 4000,
-    JWT_KEY: env.SECRET || 'akjhfdkadsjh32Efdsjkl78$^&alsdsfajh@#jkhsdfak%jhfdsa'
+    PORT:  process.env.PORT || || 4000,
+    JWT_KEY: process.env.SECRET || 'akjhfdkadsjh32Efdsjkl78$^&alsdsfajh@#jkhsdfak%jhfdsa'
 };

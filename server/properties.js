1f56cf27a0b2a1f8ac9102c4e07e5c37b433a7c4
Ahmed Mokhtar <ahmed.abdelrehim@student.guc.edu.eg>
2021-01-17 09:48:54 +0200

    Update properties.js

--- server/properties.js
+++ server/properties.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     CONNECTION_STRING: process.env.MONGODB_URI || 'mongodb+srv://admin:admin@omegacentauri.vydfb.mongodb.net/portalData?retryWrites=true&w=majority',
-    PORT:  process.env.PORT || || 4000,
+    PORT:  process.env.PORT || 4000,
     JWT_KEY: process.env.SECRET || 'akjhfdkadsjh32Efdsjkl78$^&alsdsfajh@#jkhsdfak%jhfdsa'
 };
